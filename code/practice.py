b7544c5b4418271e367af34cd1695904264db97d
Dcrisp <daniel.crisp1994@gmail.com>
2023-05-23 20:54:47 +0100

    added changes

--- code/practice.py
+++ code/practice.py
@@ -1,21 +1,15 @@
 #Challenge for data manipulation
+#import libraries
+import pandas as pd #for working with data
+from pathlib import Path #working with file paths
+import statistics #for basic statistical code eg.stdev
 
-AB7total = [100, 93, 85, 93, 95, 105, 85, 85, 86]
-AB7Ram = [91, 85, 83, 90, 93, 97, 81, 82, 84]
-ABAmo = [9, 8, 2, 3, 2, 8, 4, 3, 2]
-ABAK7total = [115, 125, 200, 102, 175, 99, 151, 136, 109, 198, 84]
-ABAK7Ram = [104, 112, 195, 98, 152, 82, 132, 121, 101, 169, 73]
-ABAK7Amo = [11, 13, 15, 4, 23, 17, 19, 15, 8, 29, 11]
+#import data
+data_file_path = Path("..", "data", "wt_ihc_microglia23.csv")
+microglia_data = pd.read_csv(data_file_path)
 
-import statistics
+#explore data
+print(microglia_data)
 
 def analytics(x):
     return ("avg =  " + str(sum(x) / len(x))), "n = " + str(len(x)), "stdev = " + str(statistics.stdev(x))
-a = analytics(AB7total)
-print(a)
-b = analytics(AB7Ram)
-print(b)
-c = analytics(ABAmo)
-print(c)
-d = analytics(ABAK7total)
-print(d)
